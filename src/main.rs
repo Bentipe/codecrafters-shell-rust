b8cb4d8ce2ff8acd3d84de93245170f099862dfa
bentipe <miguelnicolas.rm@gmail.com>
2025-10-16 21:28:23 +0200

    exit code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+#[derive(Debug, PartialEq)]
+enum Command {
+    Exit,
+    Echo,
+    UnknownCommand
+}
 
+struct ReceivedCommand {
+    command: Command,
+    arguments: Vec<String>
+}
+
+
+impl Command {
+    fn from_string(command: String) -> Self {
+        match command.as_str() {
+            "exit" => Command::Exit,
+            "echo" => Command::Echo,
+            _ => Command::UnknownCommand
+        }
+    }
+}
 
 fn main() {
     // Uncomment this block to pass the first stage
@@ -13,11 +34,25 @@ fn main() {
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
 
-        let clean_command = command.trim();
+        let structured_command: ReceivedCommand = get_the_structured_command(&command);
 
-        match clean_command {
-            "exit 0" => break,
-            _default => println!("{}: command not found", clean_command),
+        match structured_command.command {
+            Command::Echo => println!("Echo: {}", structured_command.arguments[0]),
+            Command::Exit=> break,
+            Command::UnknownCommand => println!("{}: command not found", command),
         }
+    }
+}
+
+fn get_the_structured_command(terminal_command: &String) -> ReceivedCommand {
+    let command_parts: Vec<&str> = terminal_command.split_whitespace().collect();
+    let command: Command = Command::from_string(command_parts[0].to_string());
+    let arguments: Vec<String> = command_parts[1..]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+    ReceivedCommand {
+        command,
+        arguments
     }
 }
